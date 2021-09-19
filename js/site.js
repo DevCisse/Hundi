743ce0839e4e98951a14230a9c224508443d6d1c
Hassan Salihu <45653783+DevCisse@users.noreply.github.com>
2021-09-19 11:29:48 +0100

    Initial Commit

--- js/site.js
+++ js/site.js
@@ -1,7 +1,66 @@
+document.getElementById("btnSubmit").addEventListener("click", getValues);
 
-//call hello world
+// //get the values from the page
 
-function helloWorld()
-{
-    alert('hello world');
+function getValues() {
+    //get values from the page
+
+    let startValue = document.querySelector("#startValue").value;
+    let endValue = document.querySelector("#endValue").value;
+
+
+    // we generate the values
+
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+       let  numbers = generateNumbers(startValue, endValue);
+
+       displayNumbers(numbers);
+    } else {
+        alert("You must enter a integer");
+    }
+
+}
+
+//generate the numbers from start to end
+function generateNumbers(startValue, endValue) {
+    let numbers = [];
+
+    for (let index = startValue; index <= endValue; index++) {
+        numbers.push(index);
+    }
+
+    return numbers;
+}
+
+//display the numbers and mark even numbers bold
+
+function displayNumbers(numbers) {
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+        let number = numbers[index];
+        let className = "";
+
+
+
+        if(number % 2 == 0)
+        {
+            className = "even"
+        }
+        else{
+            className = "bold";
+        }
+
+        //code does not display properly
+
+        templateRows+= `<tr class='${className}'><td>${number}</td></tr>`;
+
+        document.getElementById("results").innerHTML = templateRows;   
+    }
+
+    
 }
